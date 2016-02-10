6b5ee6aeb6f7c58224678160e691866d484ce712
Ly Nguyen <Ly900@aol.com>
2016-02-10 13:47:38 -0500

    add functions for hover and click effects for game mode

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 $(document).ready(function() {
 
-  function getLi() {
-    var clickedDiv = $("div.game-mode");
-    clickedDiv.on("click", removeActiveLiClass);
-  }
+  var gameModeUl = $("#game-mode"),
+      liMode = gameModeUl.children(".mode");
 
-  function addActiveLiClass() {
-    $(event.target).addClass("activeLi");
-  }
+  var hoveredList = function() {
+    function initHoverEffect() {
+      liMode.on("mouseover", removeHoverFromOthers);
+    };
+    function removeHoverFromOthers() {
+      gameModeUl.children(".hoverLi").removeClass("hoverLi");
+      addHoverEffect();
+    };
+    function addHoverEffect() {
+      var hoveredLi = $(event.target);
+      if (hoveredLi.hasClass("activeLi") === false) {
+        $(hoveredLi).addClass("hoverLi");
+      }
+    }
+    initHoverEffect();
+  };
 
-  function removeActiveLiClass() {
-    var clickedLi = $(event.target).hasClass(".activeLi");
-      $(this).find(".activeLi").removeClass("activeLi");
+  var clickedList = function() {
+    function getLi() {
+      gameModeUl.on("click", function() {
+        /* "this" is the ul parent clicked on */
+        var self = this;
+        removeActiveLiClass(self);
+      });
+    };
+    function removeActiveLiClass(self) {
+      /* "self" is the ul parent clicked on */
+      $(self).children(".activeLi").removeClass("activeLi");
       addActiveLiClass();
-  }
+    };
+    function addActiveLiClass() {
+      var clickedLi = $(event.target);
+      $(clickedLi).addClass("activeLi");
+    };
+    getLi();
+  };
 
-
-
-  getLi();
+  /* Initiates hover class for list hovered over */
+  hoveredList();
+  /* Initiates active class for clicked list */
+  clickedList();
 
 });
