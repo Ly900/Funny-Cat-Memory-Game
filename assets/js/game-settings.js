b0ce05bef4d903353bb2ed2031d005e9c437cee4
Ly Nguyen <Ly900@aol.com>
2016-03-03 23:08:37 -0500

    pass event into callback functions to debug click problems on mobile

--- assets/js/game-settings.js
+++ assets/js/game-settings.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     function initHoverEffect() {
       liMode.on("mouseover", removeHoverFromOthers);
     };
-    function removeHoverFromOthers() {
+    function removeHoverFromOthers(event) {
       gameModeUl.children(".hoverLi").removeClass("hoverLi");
       addHoverEffect();
     };
@@ -19,7 +19,7 @@ $(document).ready(function() {
       var hoveredLi = $(event.target);
       if (hoveredLi.hasClass("activeLi") === false) {
         $(hoveredLi).addClass("hoverLi");
-        $(hoveredLi).on("mouseleave", function() {
+        $(hoveredLi).on("mouseleave", function(event) {
           $(hoveredLi).removeClass("hoverLi");
         })
       }
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
   var clickedList = function() {
     function getLi() {
-      gameModeUl.on("click", function() {
+      gameModeUl.on("click", function(event) {
         /* "this" is the ul parent clicked on */
         var self = this;
         removeActiveLiClass(self);
@@ -57,15 +57,15 @@ $(document).ready(function() {
     function hideDir(dir) {
       $(dir).slideUp(200);
     };
-    $(easyGame).on("click", function() {
+    $(easyGame).on("click", function(event) {
       hideDir(hardDir);
       hideDir(mediumDir);
     });
-    $(mediumGame).on("click", function() {
+    $(mediumGame).on("click", function(event) {
       $(mediumDir).slideDown(200);
       hideDir(hardDir);
     });
-    $(hardGame).on("click", function() {
+    $(hardGame).on("click", function(event) {
       $("#all-dir").children().slideDown(200);
     })
   };
